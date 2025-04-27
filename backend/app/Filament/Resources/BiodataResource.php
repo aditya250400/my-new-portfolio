9ee6e463d5b298ba21f06fbe2dcadef8529e110f
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-04-27 14:26:51 +0700

    add filament resource

---
+++ backend/app/Filament/Resources/BiodataResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BiodataResource\Pages;
+use App\Filament\Resources\BiodataResource\RelationManagers;
+use App\Models\Biodata;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BiodataResource extends Resource
+{
+    protected static ?string $model = Biodata::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBiodatas::route('/'),
+            'create' => Pages\CreateBiodata::route('/create'),
+            'edit' => Pages\EditBiodata::route('/{record}/edit'),
+        ];
+    }
+}
